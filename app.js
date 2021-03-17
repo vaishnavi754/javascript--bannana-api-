346379b4be114c4c479a379de4af4a3a9e641752
vaishnavi paliya <paliyavaishnavi11@gmail.com>
2021-03-17 10:24:52 +0530

    input and console.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var btnTranslate =document.querySelector("#btn-translate");
-
-  console.log(btnTranslate)
-
-
-btnTranslate.addEventListener("click", handleClick())
+var txtInput=document.querySelector("#txt-input");
+ 
+  console.log(txtInput)
 
 function handleClick(){
     console.log(" button clicked fuction is called")
+    console.log("input", txtInput.value)
 }    
    
-   
+   btnTranslate.addEventListener("click", handleClick)
+
+
 
 
     
