f9d53d19f22233b48f74ba493a9d566e5a5ad959
vaishnavi paliya <paliyavaishnavi11@gmail.com>
2021-03-18 22:21:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var btnTranslate =document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var txtOutput=document.querySelector("#txt-output")
+
+var serverURL=""
+
+function getFinalURL(text) {
+   return serverURL +"?"+ "text" + text
+}
+
+function errorHandler(error){
+   console.log("error occured",error);
+   alert("please wait for a while there is an server issue");
+}
  
 
 function handleClick(){
- txtOutput.innerText="abcd "+txtInput.value;
-}    
+   var inputText =txtInput.value
+
+   fetch(getFinalURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+         var translatedText = json.contents.translated
+         outputDiv.innerText= translatedText
+   })
+   .catch(errorHandler)
+} 
    
    btnTranslate.addEventListener("click", handleClick)
 
